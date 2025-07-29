d8cd378b7beaa7cdf6d69610adae901309c5ccf2
techvaib.com <techvaib1@gmail.com>
2025-07-28 20:17:04 -0700

    Update enable-istio-conncetion.sh

--- enable-istio-conncetion.sh
+++ enable-istio-conncetion.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for svc in $(kubectl get svc --all-namespaces -o json | jq -r '.items[] | "\(.metadata.name) \(.metadata.namespace)"'); do
-  svc_name=$(echo $svc | awk '{print $1}')
-  svc_ns=$(echo $svc | awk '{print $2}')
+DEST_RULE_NS="istio-config"
+
+kubectl get svc --all-namespaces -o json | jq -r '
+  .items[] |
+  select(.metadata.name | startswith("hif")) |
+  "\(.metadata.name) \(.metadata.namespace)"
+' | while read -r svc_name svc_ns; do
   host="${svc_name}.${svc_ns}.svc.cluster.local"
 
   cat <<EOF > dr-${svc_name}-${svc_ns}.yaml
@@ -10,7 +14,7 @@ apiVersion: networking.istio.io/v1alpha3
 kind: DestinationRule
 metadata:
   name: ${svc_name}-dr
-  namespace: istio-config  # or $svc_ns if you prefer per-namespace
+  namespace: ${DEST_RULE_NS}
 spec:
   host: ${host}
   exportTo:
@@ -23,6 +27,15 @@ spec:
         http2MaxRequests: 500
         http1MaxPendingRequests: 100
         maxRequestsPerConnection: 50
+    outlierDetection:
+      consecutiveGatewayErrors: 5
+      interval: 10s
+      baseEjectionTime: 30s
+      maxEjectionPercent: 50
+    retries:
+      attempts: 3
+      perTryTimeout: 2s
 EOF
 
+  echo "Generated DestinationRule with targeted circuit breaker for $host"
 done
